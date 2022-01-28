393f804b4a7cf7cd4f7f7d6ceb61e5bc158fc5de
jwenger100 <jon.wenger@gmail.com>
2022-01-28 18:12:24 +0000

    2.3.5

--- src/model/AgeVerifyRequest.js
+++ src/model/AgeVerifyRequest.js
@@ -10,7 +10,7 @@
  * @author     Jonathan Wenger <jonathan.wenger@avalara.com>
  * @copyright  2004-2022 Avalara, Inc.
  * @license    https://www.apache.org/licenses/LICENSE-2.0
- * @version    2.2.9
+ * @version    2.3.5
  * @link       https://github.com/avadev/AvaTax-REST-V3-JS-SDK
  */
 
@@ -20,7 +20,7 @@ import AgeVerifyRequestAddress from './AgeVerifyRequestAddress';
 /**
  * The AgeVerifyRequest model module.
  * @module model/AgeVerifyRequest
- * @version 2.2.9
+ * @version 2.3.5
  */
 class AgeVerifyRequest {
     /**
