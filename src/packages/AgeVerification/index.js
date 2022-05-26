d75a9cce6e834f305f26f1b25474e824472716be
Jonathan Wenger <jonathan.wenger@avalara.com>
2022-05-26 09:32:26 -0700

    Updated with new package formatting.

---
+++ src/packages/AgeVerification/index.js
@@ -0,0 +1,6 @@
+import { default as AgeVerificationApi } from '../../api/AgeVerification/AgeVerificationApi.js';
+import { default as AgeVerifyFailureCode } from '../../model/AgeVerification/AgeVerifyFailureCode.js';
+import { default as AgeVerifyRequest } from '../../model/AgeVerification/AgeVerifyRequest.js';
+import { default as AgeVerifyRequestAddress } from '../../model/AgeVerification/AgeVerifyRequestAddress.js';
+import { default as AgeVerifyResult } from '../../model/AgeVerification/AgeVerifyResult.js';
+export default { AgeVerificationApi, AgeVerifyFailureCode, AgeVerifyRequest, AgeVerifyRequestAddress, AgeVerifyResult };
